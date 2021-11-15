d08b68405cb0776f9bafb1f4d4236ec4368113ad
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-11-14 21:54:19 -0700

    initial commit with fix

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <NavBar/>
+    <BookSearch/>
+    <footer></footer>>
+
+</template>
+
+<script>
+import NavBar from '../components/utilities/Nav.vue'
+import BookSearch from '../components/search/BookSearch.vue'
+export default {
+    name: 'Home', 
+    components: {
+        BookSearch,
+        NavBar
+    }
+}
+</script>
+
+<style>
+    footer {
+        height: 100px;
+        background-color: #383838;
+    }
+    
+</style>
---
+++ src/views/MyBooks.vue
@@ -0,0 +1,30 @@
+<template>
+    <NavBar/>
+    <div>
+       <h2>My Books</h2>
+       <select id="filterCategory" class="categoryPicker">
+          <option selected>Filter By Category</option>
+        </select>
+    </div>
+</template>
+
+<script>
+import NavBar from '../components/utilities/Nav.vue'
+export default {
+    name: 'About', 
+    components: {
+        NavBar
+    }
+}
+
+// const filterList = (event) => {
+//   let selectElement = event.target;
+//   let value = selectElement.value;
+//   let filteredList = toggleReadLater.filter(e => e.toggleReadLater == value) || let filteredList = toggleHaveRead.filter(e => e.toggleHaveRead == value)
+// }
+// const categoryDropDown = document.getElementById('filterCategory');
+
+</script>
+
+<style>
+</style>
